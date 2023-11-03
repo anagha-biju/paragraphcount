802e02a7ec4e804d8f6f8dd73e954d256032e4aa
Anagha Biju <bijuanagha04@gmail.com>
2023-11-03 20:28:00 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function WordCounter() {
+  const [state, setState] = useState({
+    wordCount: 0, 
+    charCount: 0
+  });
+  
+  const handleKeyPress = (e) => {
+    const count = e.target.value;
+    
+    const countWords = (count) => {
+      if (count.length === 0) {
+        return 0;
+      } else {
+        count = count.replace(/(^\s*)|(\s*$)/gi,"");
+        count = count.replace(/[ ]{2,}/gi," ");
+        count = count.replace(/\n /,"\n");
+        return count.split(' ').length; 
+      }
+    }
+    
+    setState({
+      wordCount: countWords(count),
+    });
+  }
+  
+  return(
+    <div id='container'>   
+      <h1>Responsive Paragraph Word Counter</h1> 
+      <textarea 
+        onChange={handleKeyPress}
+      >    
+      </textarea>
+      <h4>Word Count: 
+        <span className='num'> {state.wordCount}</span> 
+      </h4> 
     </div>
   );
 }
 
+function App() {
+  return (
+    <WordCounter />
+  );
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 export default App;
